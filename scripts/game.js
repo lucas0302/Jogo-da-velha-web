9977b503c9e7df1460901bcde4de85d4fa1c76f4
Lucas Araujo <lucasao744@gmail.com>
2023-07-02 19:59:13 -0300

    add funtion

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,15 @@
 //iniciar variaveis
-let board = ['','','','','','','','',''];
+let board = ['', '', '', '', '', '', '', '', ''];
 let playerTime = 0;
-let symbols = ['o','x'];
+let symbols = ['o', 'x'];
 
-function handleMove(position){
-    board[position] = symbols[playerTime];
-    if(playerTime == 0){
-        playerTime = 1;
-    }else{
-        playerTime = 0;
+function handleMove(position) {
+    if (board[position] == '') {
+        board[position] = symbols[playerTime];
+        if (playerTime == 0) {
+            playerTime = 1;
+        } else {
+            playerTime = 0;
+        }
     }
 }
--- scripts/interface.js
+++ scripts/interface.js
@@ -1,10 +1,27 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     let squares = document.querySelectorAll(".square");
-    squares.forEach((square)=>{
-        square.addEventListener('click',handleClick);
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
     })
 })
-function handleClick(event){
-    console.log(event.target);
+function handleClick(event) {
+    let square = event.target;
+    let position = square.id;
+
+    handleMove(position);
+    updateSquares();
+
 }
+
+function updateSquares() {
+    let squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+        if (symbol != ''){
+            square.innerHTML = `<div class= "${Symbol}"></div>`
+        }
+    })
+}
+
