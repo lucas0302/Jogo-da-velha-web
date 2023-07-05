506a9868ad3e08e97875db3ed306c9cbe0f2ce07
Lucas Araujo <lucasao744@gmail.com>
2023-07-04 21:34:43 -0300

    terminando o jogo da velha

--- scripts/interface.js
+++ scripts/interface.js
@@ -9,19 +9,18 @@ function handleClick(event) {
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
+    if (handleMove(position)) {
+        setTimeout(() => {
+            alert("O jogo acabou e o jogador " + playerTime + " foi o vencedor");
+        }, 10);
 
+    };
+    updateSquare(position);
+}
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let Symbols = board[position];
+    square.innerHTML = `<div class= '${Symbols}'></div>`
 }
 
-function updateSquares() {
-    let squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-        let position = square.id;
-        let symbol = board[position];
-        if (symbol != ''){
-            square.innerHTML = `<div class= "${Symbol}"></div>`
-        }
-    })
-}
 
